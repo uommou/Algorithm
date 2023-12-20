e1ab2602d98a47a6984ec762d1bb35ed54214048
uommou <90598552+uommou@users.noreply.github.com>
2023-12-20 13:05:14 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    vector<int> dp(N, 1);
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            if (A[j] < A[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    cout << *max_element(dp.begin(), dp.end()) << endl;
+    return 0;
+}
