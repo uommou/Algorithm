3e1aa2353b2603b454837042a7924f0a42640ea7
uommou <90598552+uommou@users.noreply.github.com>
2024-02-13 17:03:56 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 316 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -4,65 +4,59 @@
 
 using namespace std;
 
-int lower_bound(const vector<int>& array, int target){
-    int low =0;
-    int high = array.size();
-    while (low < high){
-        int mid = low + (high - low)/2;
-        if (array[mid] >= target){
+int lower_bound(const vector<int>& array, int target) {
+    int low = 0, high = array.size();
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (array[mid] >= target) {
             high = mid;
-        }
-        else {
+        } else {
             low = mid + 1;
         }
     }
     return low;
 }
 
-int upper_bound(const vector<int>& array, int target){
-    int low = 0;
-    int high = array.size();
-    while (low < high){
-        int mid = low + (high - low)/2;
-        if (array[mid] <= target){
+int upper_bound(const vector<int>& array, int target) {
+    int low = 0, high = array.size();
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (array[mid] <= target) {
             low = mid + 1;
-        }
-        else {
+        } else {
             high = mid;
         }
     }
     return low;
 }
 
-int count_cards(const vector<int>& cards, int number){
+int count_cards(const vector<int>& cards, int number) {
     int lower = lower_bound(cards, number);
     int upper = upper_bound(cards, number);
     return upper - lower;
 }
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     int n, m;
     cin >> n;
     vector<int> cards(n);
-    for (int i=0; i< n; ++i){
+    for (int i = 0; i < n; ++i) {
         cin >> cards[i];
     }
-    
     sort(cards.begin(), cards.end());
-    
+
     cin >> m;
     vector<int> queries(m);
-    for (int i=0; i< m; ++i){
+    for (int i = 0; i < m; ++i) {
         cin >> queries[i];
     }
-    
-    for (int number : queries){
+
+    for (int number : queries) {
         cout << count_cards(cards, number) << " ";
     }
-    
+
     return 0;
 }
