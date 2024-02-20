edb26e4d2488de74137f3eaafdabd2953b759dad
uommou <90598552+uommou@users.noreply.github.com>
2024-02-21 06:36:03 +0900

    [Gold IV] Title: 최단경로, Time: 316 ms, Memory: 8996 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,49 +1,63 @@
+//
+// Created by iw040 on 2021-11-02.
+//
+
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <limits>
+
 using namespace std;
+typedef pair<int, int> ci;
+const int INF = 1e5 * 2; //최대 V-1개의 간선을 지나게 됨
 
-const int INF = numeric_limits<int>::max();
-typedef pair<int, int> pii;
+//다익스트라
+vector<int> dijkstra(int vertex, int start, vector<vector<ci>> &graph) {
+    vector<int> dist(vertex + 1, INF);
+    priority_queue<ci, vector<ci>, greater<>> pq; //first : 시작점으로부터의 거리, second : 정점
 
-int main() {
-    int V, E, K;
-    cin >> V >> E >> K;
-    vector<vector<pii>> graph(V + 1);
-    vector<int> distance(V + 1, INF);
-
-    for (int i = 0; i < E; i++) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        graph[u].push_back({v, w});
-    }
-
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    pq.push({0, K});
-    distance[K] = 0;
+    //시작 위치 초기화
+    dist[start] = 0;
+    pq.push({0, start});
 
     while (!pq.empty()) {
-        int dist = pq.top().first;
-        int current = pq.top().second;
+        int weight = pq.top().first;
+        int node = pq.top().second;
         pq.pop();
 
-        if (distance[current] < dist) continue;
-
-        for (auto& edge : graph[current]) {
-            int next = edge.first;
-            int nextDist = dist + edge.second;
-            if (nextDist < distance[next]) {
-                distance[next] = nextDist;
-                pq.push({nextDist, next});
+        if (weight > dist[node]) //이미 확인했던 정점
+            continue;
+        for (int i = 0; i < graph[node].size(); i++) {
+            int next_node = graph[node][i].first; //연결된 정점
+            int next_weight = weight + graph[node][i].second; //시작점으로부터 연결된 정점까지의 거리
+            if (dist[next_node] > next_weight) { //더 짧은 경로로 갈 수 있다면
+                dist[next_node] = next_weight;
+                pq.push({next_weight, next_node});
             }
         }
     }
+    return dist;
+}
 
-    for (int i = 1; i <= V; i++) {
-        if (distance[i] == INF) cout << "INF\n";
-        else cout << distance[i] << '\n';
+int main() {
+    int vertex, edge, k, u, v, w;
+
+    //입력
+    cin >> vertex >> edge >> k;
+    vector<vector<ci>> graph(vertex + 1, vector<ci>(0)); //인접 리스트
+    while (edge--) {
+        cin >> u >> v >> w;
+        graph[u].emplace_back(v, w); //방향 그래프
     }
 
-    return 0;
+    //연산
+    vector<int> ans = dijkstra(vertex, k, graph);
+
+    //출력
+    for (int i = 1; i <= vertex; i++) {
+        if (ans[i] == INF)
+            cout << "INF";
+        else
+            cout << ans[i];
+        cout << '\n';
+    }
 }
