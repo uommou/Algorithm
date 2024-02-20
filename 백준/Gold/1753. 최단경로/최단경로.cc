b800e01bb205ce664f6f1c7d614c1f971c1827da
uommou <90598552+uommou@users.noreply.github.com>
2024-02-21 07:07:32 +0900

    [Gold IV] Title: 최단경로, Time: 100 ms, Memory: 9112 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,65 +1,59 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <climits> // INT_MAX 사용
-
+#include <limits>
 using namespace std;
 
-typedef pair<int, int> Edge; // (가중치, 도착 정점)
-typedef vector<vector<Edge>> AdjacencyList;
+const int INF = numeric_limits<int>::max();
+typedef pair<int, int> pii;
+typedef vector<vector<pii>> Graph;
 
-vector<int> DijkstraShortestPath(const AdjacencyList& graph, int source_vertex) {
-    vector<int> min_distance(graph.size(), INT_MAX);
-    min_distance[source_vertex] = 0;
+vector<int> dijkstra(const Graph& graph, int src) {
+    int n = graph.size();
+    vector<int> dist(n, INF);
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    
+    dist[src] = 0;
+    pq.push({0, src});
 
-    priority_queue<Edge, vector<Edge>, greater<Edge>> min_heap;
-    min_heap.push({0, source_vertex});
+    while (!pq.empty()) {
+        int currentDist = pq.top().first;
+        int current = pq.top().second;
+        pq.pop();
 
-    while (!min_heap.empty()) {
-        int current_vertex = min_heap.top().second;
-        int distance_to_current = min_heap.top().first;
-        min_heap.pop();
+        if (dist[current] < currentDist) continue;
 
-        if (distance_to_current > min_distance[current_vertex]) continue;
-
-        for (const Edge& adjacent_edge : graph[current_vertex]) {
-            int adjacent_vertex = adjacent_edge.second;
-            int weight_to_adjacent = adjacent_edge.first;
-
-            int potential_distance = min_distance[current_vertex] + weight_to_adjacent;
-            if (potential_distance < min_distance[adjacent_vertex]) {
-                min_distance[adjacent_vertex] = potential_distance;
-                min_heap.push({potential_distance, adjacent_vertex});
+        for (auto& edge : graph[current]) {
+            int next = edge.first, nextDist = currentDist + edge.second;
+            if (dist[next] > nextDist) {
+                dist[next] = nextDist;
+                pq.push({nextDist, next});
             }
         }
     }
 
-    return min_distance;
+    return dist;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false); // 입출력 속도 향상
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     int V, E, K;
     cin >> V >> E >> K;
-
-    AdjacencyList graph(V+1); // 정점 번호가 1부터 시작하므로 V+1 크기 할당
+    Graph graph(V + 1);
 
     for (int i = 0; i < E; ++i) {
         int u, v, w;
         cin >> u >> v >> w;
-        graph[u].push_back({w, v}); // u에서 v로 가는 가중치 w인 간선 추가
+        graph[u].push_back({v, w});
     }
 
-    vector<int> min_distance = DijkstraShortestPath(graph, K); // K번 정점에서 시작
+    vector<int> min_distance = dijkstra(graph, K);
 
-    for (int vertex = 1; vertex < min_distance.size(); ++vertex) {
-        if (min_distance[vertex] == INT_MAX) {
-            cout << "INF\n";
-        } else {
-            cout << min_distance[vertex] << "\n";
-        }
+    for (int i = 1; i <= V; ++i) {
+        if (min_distance[i] == INF) cout << "INF\n";
+        else cout << min_distance[i] << "\n";
     }
 
     return 0;
